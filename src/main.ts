616777015cb5c3764f9f68eb2b695b3800b219c8
Juan Lee <ninejuan@protonmail.com>
2024-08-26 05:58:33 +0000

    add: main page swagger, cors, helmet, db conn

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,40 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
-import * as passport from 'passport';
+import { setupSwagger } from './utils/swagger.util';
+import { linkToDatabase } from './utils/db.util';
+import { config } from 'dotenv';
+import helmet from 'helmet';
+
+config(); const env = process.env;
+const logger = new Logger('Service-backend');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.use(
-    session({
-      secret: '',
-      saveUninitialized: false,
-      resave: false,
-      cookie: {
-        maxAge: 60000,
-      },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
-  await app.listen(3001);
+  app.setGlobalPrefix(env.GLOBAL_PREFIX);
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    exposedHeaders: ["Authorization"]
+  });
+
+  app.use(helmet({
+    contentSecurityPolicy: false
+  }));
+  
+  await linkToDatabase();
+  if (env.MODE == "DEV") {
+    try {
+      setupSwagger(app);
+      logger.log("Swagger is enabled");
+    } catch (e) {
+      logger.error(e);
+    }
+  }
+  await app.listen(env.PORT || 3000).then(() => {
+    console.log(`App is running on Port ${env.PORT || 3000}`)
+  }).catch((e) => {
+    console.error(e)
+  });
 }
 bootstrap();
