eae77279a417352e1e6f0711ba5298078bf7004f
Juan Lee <ninejuan@protonmail.com>
2024-08-29 06:13:02 +0000

    update: create user dto

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -3,8 +3,8 @@ import mongo from 'mongoose';
 const userSchema = new mongo.Schema({
     google_mail: { type: String, required: true },
     nickname: { type: String, required: true },
-    description: { type: String, default: "" },
-    associated: { type: String, default: "" },
+    description: { type: String, default: " " },
+    associated: { type: String, default: "무소속" },
     profilePhoto: { type: String, default: "default.png" }, // 저장된 파일명
     refreshToken: { type: String, default: "" },
 });
--- src/res/auth/dto/create-user.dto.ts
+++ src/res/auth/dto/create-user.dto.ts
@@ -2,26 +2,38 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAuthDto {
     @ApiProperty({
-        description: "유저의 id입니다. 가입 시 사용합니다.",
-        example: "ninejuan"
+        description: "유저의 Google E-mail Address입니다. 가입 시 사용합니다.",
+        example: "juan.lee@octive.net"
     })
-    id!: string;
+    google_mail!: string;
 
     @ApiProperty({
-        description: "유저의 비밀번호입니다.",
-        example: "examplePW"
-    })
-    pw!: string;
-
-    @ApiProperty({
-        description: "유저의 닉네임입니다. (기본값 : 유저 id)",
-        example: "나인주안"
+        description: "유저의 닉네임입니다. 기본적으로 Google display_name 속성이 설정됩니다.",
+        example: "주안 이"
     })
     nickname: string;
 
     @ApiProperty({
-        description: "유저의 이메일 주소입니다. (이메일 인증 필요)",
-        example: "hello.world@octive.net"
+        description: "유저 소개글입니다. 기본적으로 비어있는 값을 저장합니다.",
+        example: "선린인터넷고등학교에서 소프트웨어 개발을 전공하고 있는 이주안입니다."
     })
-    email!: string;
+    description?: string;
+
+    @ApiProperty({
+        description: "유저의 소속입니다. 기본적으로 무소속으로 저장됩니다.",
+        example: "Octive"
+    })
+    associated?: string;
+
+    @ApiProperty({
+        description: "유저의 프로필 사진입니다. 변경이 불가하며, 구글 계정의 프로필 사진이 지정됩니다.",
+        example: "https://example.com/google_profile.png"
+    })
+    profilePhoto: string;
+
+    @ApiProperty({
+        description: "유저의 refresh Token입니다.",
+        example: "kfjhf84h8f="
+    })
+    refreshToken: string;
 }
